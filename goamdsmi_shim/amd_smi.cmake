edeb9981c0d8bb883b0a479429c9f5fb8b310291
root123 <root123@localhost.localdomain>
2024-06-25 19:52:42 +0800

    Rocm path changes

--- goamdsmi_shim/amd_smi.cmake
+++ goamdsmi_shim/amd_smi.cmake
@@ -6,7 +6,7 @@ set(CMAKE_C_COMPILER "gcc" CACHE PATH "")
 # cpp compiler
 set(CMAKE_CXX_COMPILER "g++" CACHE PATH "")
 
-set(WITH_ESMI ON CACHE BOOL "")
+set(WITH_ESMI OFF CACHE BOOL "")
 set(WITH_ROCM_SMI ON CACHE BOOL "")
 
 # path to global esmi and rocm_smi install
